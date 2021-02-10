03bc163ed3e15a18c9940e79b7ee432add60b7d1
thorerik88 <thorerik88@hotmail.com>
2021-02-10 01:25:04 +0100

    needs to return to original content when input value is empty

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@ import handleClick from "./utils/handleClick.js";
 
 const container = document.querySelector(".article-container");
 const message = document.querySelector(".message-container");
+const input = document.querySelector("#search-box");
+
+let search = false;
 
 // retrieve API on page load
 (async function() {
@@ -17,7 +20,43 @@ const message = document.querySelector(".message-container");
 
         json.forEach((article) => {
 
-            const fav = getFromStorage();
+            createHtml(article, false);
+
+            
+
+            input.addEventListener("keyup", (e) => {
+                const inputValue = e.target.value;
+
+                if (inputValue.length === 0) {
+                    createHtml(article, false)
+                }
+
+                if (inputValue.length > 0 && article.title.toLowerCase().startsWith(inputValue.trim().toLowerCase())) {
+                    createHtml(article, true);
+                } 
+            })
+        })
+        
+        const favButton = document.querySelectorAll(".fav-icon");
+        favButton.forEach((button) => {
+            button.addEventListener("click", function(e) {
+                handleClick(e.target);
+                handleStorage(e.target.dataset.id)
+            })
+        })
+        
+
+       
+
+    }
+    catch(error) {
+
+    }
+})();
+
+function createHtml(article, search) {
+
+    const fav = getFromStorage();
             let favClass = "fa fa-star-o";
             if (fav !== null) {
                 fav.forEach((item) => {
@@ -27,27 +66,30 @@ const message = document.querySelector(".message-container");
                 }) 
             }
             
+            if (search === false) {
+                container.innerHTML += `
+                    <div class="col-4 article">
+                        <h1>Title: ${article.title}</h1>
+                        <span class="fav-icon"><i class="${favClass}" data-id="${article.id}"></i></span>
+                        <h2>Summary:</h2>
+                        <p>${article.summary}</p>
+                        <h2>Author:</h2>
+                        <p>${article.author}</p>
+                    </div>
+                `;
+            } 
+            if (search === true) {
+                container.innerHTML = `
+                    <div class="col-4 article">
+                        <h1>Title: ${article.title}</h1>
+                        <span class="fav-icon"><i class="${favClass}" data-id="${article.id}"></i></span>
+                        <h2>Summary:</h2>
+                        <p>${article.summary}</p>
+                        <h2>Author:</h2>
+                        <p>${article.author}</p>
+                    </div>
+                `;
+            }
 
-            container.innerHTML += `
-            <div class="col-4 article">
-                <h1>Title: ${article.title}</h1>
-                <span class="fav-icon"><i class="${favClass}" data-id="${article.id}"></i></span>
-                <h2>Summary:</h2>
-                <p>${article.summary}</p>
-                <h2>Author:</h2>
-                <p>${article.author}</p>
-            </div>
-            `;
-        })
-        const favButton = document.querySelectorAll(".fav-icon");
-        favButton.forEach((button) => {
-            button.addEventListener("click", function(e) {
-                handleClick(e.target);
-                handleStorage(e.target.dataset.id)
-            })
-        })
-    }
-    catch(error) {
-
-    }
-})();
+            
+}
